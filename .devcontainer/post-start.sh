73a5d17d170e94dafba751102d0648aaa1fa23fe
Roy Cornelissen <roy_cornelissen@hotmail.com>
2024-11-07 08:50:02 +0100

    🧑🏻‍🔧 Attendee provisioning

--- .devcontainer/post-start.sh
+++ .devcontainer/post-start.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
 
+my_codespace=$(gh codespace list -R XpiritCommunityEvents/LegacyLiftOffWorkshop --json name | jq -r '.[0].name')
+gh issue create --assignee roycornelissen,vriesmarcel --title $my_codespace --label "provisioning" --body "Please provision my codespace" -R XpiritCommunityEvents/LegacyLiftOffWorkshop
+
 # pull and start the HMS database
 docker run -d -p 1433:1433 marcelv/project_hms_db
+
