6ec21b7e5b127b52269b160b7ba92f9bdefeb025
Roy Cornelissen <roy_cornelissen@hotmail.com>
2024-11-09 20:48:42 +0000

    🧑‍💻 Fix port visibility for forwarded port 7219 at post-create.sh

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -2,3 +2,5 @@
 
 my_codespace=$(gh codespace list -R XpiritCommunityEvents/LegacyLiftOffWorkshop --json name | jq -r '.[0].name')
 gh issue create --assignee roycornelissen,vriesmarcel --title $my_codespace --label "provisioning" --body "Please provision my codespace" -R XpiritCommunityEvents/LegacyLiftOffWorkshop
+
+gh codespace ports visibility 7219:public --codespace $my_codespace
